e4e54d61408ada7c5f73849c62cb6a75771a77a8
marziply <contact@marziply.dev>
2021-12-21 22:25:14 +0000

    Added enums

--- README.md
+++ README.md
@@ -6,3 +6,21 @@
 - Tokenisation of syntax
 - Translate tokenised syntax into Rust
 - Output rendered Rust
+
+## Available tokens
+
+### Kind
+
+- service
+- message
+
+### Meta
+
+- syntax
+- package
+
+### Scalar
+
+- int32
+- bool
+- string
--- src/parser.rs
+++ src/parser.rs
@@ -1,5 +1,21 @@
 use std::error::Error;
 
+pub enum Kind {
+  Service,
+  Message
+}
+
+pub enum Meta {
+  Syntax,
+  Package
+}
+
+pub enum Scalar {
+  Int32,
+  Bool,
+  r#String
+}
+
 pub fn parse(input: String) -> Result<String, Box<dyn Error>> {
   Ok(String::from("foo"))
 }
