4f561e64a21f4132fe2fced4ca96c34f34166f8a
marziply <contact@marziply.dev>
2021-12-31 14:04:22 +0000

    Tidied up example2

--- assets/example2.rs
+++ assets/example2.rs
@@ -1,52 +1,84 @@
+#![allow(unknown_lints)]
+#![allow(clippy::all)]
+#![allow(box_pointers)]
+#![allow(dead_code)]
+#![allow(missing_docs)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+#![allow(non_upper_case_globals)]
+#![allow(trivial_casts)]
+#![allow(unsafe_code)]
+#![allow(unused_imports)]
+#![allow(unused_results)]
+
+use super::message::{HelloReply, HelloRequest};
+use grpcio::{
+  pb_de as de,
+  pb_ser as ser,
+  CallOption,
+  Channel,
+  Client,
+  Marshaller,
+  Method,
+  MethodType,
+  RpcContext,
+  ServiceBuilder,
+  UnarySink
+};
+
+const GREETER_SAY_HELLO: Method<HelloRequest, HelloReply> = Method {
+  ty: MethodType::Unary,
+  name: "/pbrs.Greeter/SayHello",
+  req_mar: Marshaller {
+    ser,
+    de
+  },
+  resp_mar: Marshaller {
+    ser,
+    de
+  }
+};
+
 #[derive(Clone)]
 pub struct GreeterClient {
-  client: ::grpcio::Client
+  client: Client
 }
 
 impl GreeterClient {
-  pub fn new(channel: ::grpcio::Channel) -> Self {
-    GreeterClient {
-      client: ::grpcio::Client::new(channel)
+  pub fn new(channel: Channel) -> Self {
+    Self {
+      client: Client::new(channel)
     }
   }
 
   pub fn say_hello_opt(
     &self,
-    req: &super::message::HelloRequest,
-    opt: ::grpcio::CallOption
-  ) -> ::grpcio::Result<super::message::HelloReply> {
+    req: &HelloRequest,
+    opt: CallOption
+  ) -> Result<HelloReply> {
     self
       .client
-      .unary_call(&METHOD_GREETER_SAY_HELLO, req, opt)
+      .unary_call(&GREETER_SAY_HELLO, req, opt)
   }
 
-  pub fn say_hello(
-    &self,
-    req: &super::message::HelloRequest
-  ) -> ::grpcio::Result<super::message::HelloReply> {
-    self.say_hello_opt(req, ::grpcio::CallOption::default())
+  pub fn say_hello(&self, req: &HelloRequest) -> Result<HelloReply> {
+    self.say_hello_opt(req, CallOption::default())
   }
 }
 
 pub trait Greeter {
   fn say_hello(
     &mut self,
-    ctx: ::grpcio::RpcContext,
-    _req: super::message::HelloRequest,
-    sink: ::grpcio::UnarySink<super::message::HelloReply>
+    ctx: RpcContext,
+    req: HelloRequest,
+    sink: UnarySink<HelloReply>
   ) {
-    grpcio::unimplemented_call!(ctx, sink)
+    unimplemented_call!(ctx, sink)
   }
 }
 
-pub fn create_greeter<S: Greeter + Send + Clone + 'static>(
-  s: S
-) -> ::grpcio::Service {
-  let mut builder = ::grpcio::ServiceBuilder::new();
-  let mut instance = s;
-  builder = builder
-    .add_unary_handler(&METHOD_GREETER_SAY_HELLO, move |ctx, req, resp| {
-      instance.say_hello(ctx, req, resp)
-    });
-  builder.build()
+pub fn create_greeter<S: Greeter + Send + Clone + 'static>(s: S) -> Service {
+  ServiceBuilder::new()
+    .add_unary_handler(&GREETER_SAY_HELLO, s.say_hello)
+    .build()
 }
